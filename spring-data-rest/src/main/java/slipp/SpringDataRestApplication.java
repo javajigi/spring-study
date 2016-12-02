3581aa308c0827e52a3eea9b7aea7bfa7be6c573
javajigi <javajigi@gmail.com>
2016-12-02 16:12:48 +0900

    asciidoc 추가 jackson 라이브러리 설정 추가 MockMvc 예제 추가.

--- spring-data-rest/src/main/java/slipp/SpringDataRestApplication.java
+++ spring-data-rest/src/main/java/slipp/SpringDataRestApplication.java
@@ -1,12 +1,34 @@
 package slipp;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class SpringDataRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataRestApplication.class, args);
+	}
+	
+	@Bean
+	public Jackson2ObjectMapperBuilder jacksonBuilder() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder() {
+			@Override
+			public void configure(ObjectMapper mapper) {
+				mapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
+				mapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+				mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+			}
+		};
+		builder.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+		return builder;
 	}
 }

01bdeb7418d62223095c33f40701ca500c08f045
javajigi <javajigi@gmail.com>
2017-02-07 15:40:30 +0900

    refactoring validation error message

--- spring-handlebars/src/main/java/slipp/validate/ValidationExceptionControllerAdvice.java
+++ spring-handlebars/src/main/java/slipp/validate/ValidationExceptionControllerAdvice.java
@@ -1,6 +1,7 @@
 package slipp.validate;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,27 +14,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ValidationExceptionController {
-    private static final Logger log = LoggerFactory.getLogger(ValidationExceptionController.class);
-    
+public class ValidationExceptionControllerAdvice {
+    private static final Logger log = LoggerFactory.getLogger(ValidationExceptionControllerAdvice.class);
+
     @Autowired
     private MessageSourceAccessor msa;
-    
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ValidationErrorsResponse handleValidationException(MethodArgumentNotValidException exception) {
         List<ObjectError> errors = exception.getBindingResult().getAllErrors();
         ValidationErrorsResponse response = new ValidationErrorsResponse();
         for (ObjectError objectError : errors) {
-            FieldError fieldError = (FieldError)objectError;
+            log.debug("object error : {}", objectError);
+            FieldError fieldError = (FieldError) objectError;
             response.addValidationError(new ValidationError(fieldError.getField(), getErrorMessage(fieldError)));
         }
         return response;
     }
 
     private String getErrorMessage(FieldError fieldError) {
-        String[] codes = fieldError.getCodes();
-        String errorMessage = msa.getMessage(codes[0], fieldError.getArguments(), fieldError.getDefaultMessage());
+        Optional<String> code = getFirstCode(fieldError);
+        if (!code.isPresent()) {
+            return null;
+        }
+
+        String errorMessage = msa.getMessage(code.get(), fieldError.getArguments(), fieldError.getDefaultMessage());
         log.info("error message: {}", errorMessage);
         return errorMessage;
+    }
+
+    private Optional<String> getFirstCode(FieldError fieldError) {
+        String[] codes = fieldError.getCodes();
+        if (codes == null || codes.length == 0) {
+            return Optional.empty();
+        }
+        return Optional.of(codes[0]);
     }
 }

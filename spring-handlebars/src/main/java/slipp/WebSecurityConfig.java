2ed86d8d47f18292b5e363b1ce7a662d3df5587a
javajigi <javajigi@gmail.com>
2017-02-07 17:53:33 +0900

    separate security config

--- spring-handlebars/src/main/java/slipp/WebSecurityConfig.java
+++ spring-handlebars/src/main/java/slipp/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package slipp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,47 +12,81 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
-import slipp.security.CustomUserDetailsService;
+public abstract class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-@Configuration
-@EnableWebSecurity
-@ComponentScan(basePackageClasses = CustomUserDetailsService.class)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService); //.passwordEncoder(passwordencoder());
+    }
+    
+    abstract void configureCsrf(HttpSecurity http) throws Exception;
+    
+    abstract void configureHttpBasic(HttpSecurity http) throws Exception;
 
-	@Autowired
-	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService); //.passwordEncoder(passwordencoder());
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        configureCsrf(http);
+        configureHttpBasic(http);
+        
+        http.authorizeRequests()
+            .antMatchers("/css/**").permitAll();
+        
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/hello").access("hasRole('ROLE_USER')")
+                .antMatchers("/boards/**").authenticated()
+                .anyRequest().permitAll()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+            .and()
+                .logout()
+                .logoutSuccessUrl("/login?logout")
+            .and()
+                .exceptionHandling()
+                .accessDeniedPage("/403");
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-		  .csrf().disable();
-		
-		http.authorizeRequests()
-			.antMatchers("/css/**").permitAll();
-		
-		http.authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/hello").access("hasRole('ROLE_USER')")
-				.antMatchers("/boards/**").authenticated()
-				.anyRequest().permitAll()
-			.and()
-				.formLogin()
-				.loginPage("/login")
-				.usernameParameter("username")
-				.passwordParameter("password")
-			.and()
-				.logout()
-				.logoutSuccessUrl("/login?logout")
-			.and()
-				.exceptionHandling()
-				.accessDeniedPage("/403");
-	}
+//  @Bean(name = "passwordEncoder")
+//  public PasswordEncoder passwordencoder() {
+//      return new BCryptPasswordEncoder();
+//  }
+    
+    @Configuration
+    @EnableWebSecurity
+    @Profile({"local", "production"})
+    static class ProductionWebSecurityConfig extends WebSecurityConfig {
+        private static final Logger log = LoggerFactory.getLogger(WebSecurityConfig.ProductionWebSecurityConfig.class);
+        
+        @Override
+        void configureCsrf(HttpSecurity http) throws Exception {
+            log.info("disable csrf local, production profile");
+            http.csrf().disable();
+        }
 
-//	@Bean(name = "passwordEncoder")
-//	public PasswordEncoder passwordencoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+        @Override
+        void configureHttpBasic(HttpSecurity http) throws Exception {
+        }
+    }
+    
+    @Configuration
+    @EnableWebSecurity
+    @Profile("test")
+    static class TestWebSecurityConfig extends WebSecurityConfig {
+        private static final Logger log = LoggerFactory.getLogger(WebSecurityConfig.TestWebSecurityConfig.class);
+        
+        @Override
+        void configureCsrf(HttpSecurity http) throws Exception {
+            log.info("disable csrf test profile");
+            http.csrf().disable();
+        }
+
+        @Override
+        void configureHttpBasic(HttpSecurity http) throws Exception {
+            http.httpBasic();
+        }
+    }
 }
--- spring-handlebars/src/test/java/slipp/web/HelloControllerTest.java
+++ spring-handlebars/src/test/java/slipp/web/HelloControllerTest.java
@@ -1,5 +1,7 @@
 package slipp.web;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,10 +12,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class HelloControllerTest {
     private static final Logger log = LoggerFactory.getLogger(HelloControllerTest.class);
     
@@ -21,19 +25,14 @@ public class HelloControllerTest {
     
     @Before
     public void setup() {
-        template.withBasicAuth("javajigi", "test");
+        template = template.withBasicAuth("javajigi", "test");
     }
     
     @Test
     public void hello() {
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("username", "javajigi");
-//        params.add("password", "test");
-//        ResponseEntity<String> loginResult = template.postForEntity("/login", params, String.class);
-//        HttpHeaders headers = loginResult.getHeaders();
-//        log.debug("cookies : {}", headers.get("Set-Cookie"));
         ResponseEntity<String> result = template.getForEntity("/hello", String.class);
         log.debug("body : {}", result.getBody());
+        assertTrue(result.getBody().contains("/logout"));
     }
 
 }

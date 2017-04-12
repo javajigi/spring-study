2f47d8493de6ff810ad2e9240f2b53ba942bd28c
Daekwon Kang <daekwon.kang@gmail.com>
2017-04-12 21:46:15 +0900

    이전 scale 갯수 유지
    blue -> green 변경될때 기존 scale 갯수를 유지해서 배포되도록 수정

--- my-spring-session/redeploy.sh
+++ my-spring-session/redeploy.sh
@@ -10,18 +10,24 @@ docker-compose up -d redis
 docker tag my-spring-session:1.0.0 my-spring-session:latest
 
 EXIST_BLUE=$(docker-compose ps | grep blue | grep Up)
-
+EXIST_SCALE=$(docker ps | grep my-slipp | grep Up | wc -l | bc)
+	
 if [ -z "$EXIST_BLUE" ]; then
 	echo "deploy blue"
 	docker-compose up -d my-slipp-blue
-	# docker-compose scale my-slipp-blue=2
+	
+	if [[ $EXIST_SCALE -gt 0 ]]; then
+		docker-compose scale my-slipp-blue=$EXIST_SCALE		
+	fi
 	
 	sleep 5
 	docker-compose stop my-slipp-green
 else
 	echo "deploy green"
 	docker-compose up -d my-slipp-green
-	# docker-compose scale my-slipp-green=2
+	if [[ $EXIST_SCALE -gt 0 ]]; then
+		docker-compose scale my-slipp-green=$EXIST_SCALE		
+	fi
 	
 	sleep 5
 	docker-compose stop my-slipp-blue

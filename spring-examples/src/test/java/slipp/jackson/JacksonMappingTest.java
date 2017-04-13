7bb72c99dd90e6aa424b0a3284d5c5b43f0aef8e
javajigi <javajigi@gmail.com>
2017-04-13 15:48:00 +0900

    add jackson simple example

--- spring-examples/src/test/java/slipp/jackson/JacksonMappingTest.java
+++ spring-examples/src/test/java/slipp/jackson/JacksonMappingTest.java
@@ -2,6 +2,10 @@ package slipp.jackson;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,17 +15,29 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JacksonMappingTest {
 	private static final Logger log = LoggerFactory.getLogger(JacksonMappingTest.class);
+	
+	private ObjectMapper mapper;
+	
+	@Before
+	public void setup() {
+		mapper = new ObjectMapper();
+		mapper.registerModule(new JavaTimeModule());
+	}
 
 	@Test
 	public void mapping() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule(new JavaTimeModule());
-//		mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
 		MyData data = new MyData("Jae Sung");
 		String contents = mapper.writeValueAsString(data);
 		log.debug("contents : {}", contents);
 		MyData result = mapper.readValue(contents, MyData.class);
 		assertEquals(data, result);
 	}
-
+	
+	@Test
+	public void simple() throws Exception {
+		Map<String, String> data = new HashMap<>();
+		data.put("name", "jaesung");
+		String result = mapper.writeValueAsString(data);
+		log.debug("result : {}", result);
+	}
 }
